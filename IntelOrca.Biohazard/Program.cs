2d8222f8b8770cf9973d5ff94ac135779d5d2ed0
Ted John <ted@brambles.org>
2023-02-04 11:29:43 +0000

    Increment version to 2.2.0 and display git hash

--- IntelOrca.Biohazard/Program.cs
+++ IntelOrca.Biohazard/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 
 namespace IntelOrca.Biohazard
 {
     public class Program
     {
-        public static Version CurrentVersion = Assembly.GetEntryAssembly()?.GetName().Version ?? new Version();
+        public static Assembly CurrentAssembly => Assembly.GetEntryAssembly();
+        public static Version CurrentVersion = CurrentAssembly.GetName().Version ?? new Version();
         public static string CurrentVersionNumber => $"{CurrentVersion.Major}.{CurrentVersion.Minor}.{CurrentVersion.Build}";
-        public static string CurrentVersionInfo => $"BioRand {CurrentVersion.Major}.{CurrentVersion.Minor}.{CurrentVersion.Build}";
+        public static string CurrentVersionInfo => $"BioRand {CurrentVersion.Major}.{CurrentVersion.Minor}.{CurrentVersion.Build} ({GitHash})";
+        public static string GitHash
+        {
+            get
+            {
+                var attribute = CurrentAssembly
+                    .GetCustomAttributes<AssemblyInformationalVersionAttribute>()
+                    .FirstOrDefault();
+                var rev = attribute.InformationalVersion;
+                var plusIndex = rev.IndexOf('+');
+                if (plusIndex != -1)
+                {
+                    return rev.Substring(plusIndex + 1);
+                }
+                return rev;
+            }
+        }
     }
 }
