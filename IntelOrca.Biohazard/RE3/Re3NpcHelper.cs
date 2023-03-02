7b94aedcd0f91cdc1c0aa42bdad9950941568046
Ted John <ted@brambles.org>
2023-03-02 23:41:33 +0000

    Implement custom NPCs for RE3

--- IntelOrca.Biohazard/RE3/Re3NpcHelper.cs
+++ IntelOrca.Biohazard/RE3/Re3NpcHelper.cs
@@ -47,8 +47,6 @@ namespace IntelOrca.Biohazard.RE3
                 Re3EnemyIds.MarvinBranagh2,
                 Re3EnemyIds.NikolaiZinoviev,
                 Re3EnemyIds.BradVickers,
-                Re3EnemyIds.DarioRosso,
-                Re3EnemyIds.MurphySeeker,
                 Re3EnemyIds.TyrellPatrick,
                 Re3EnemyIds.PromoGirl,
                 Re3EnemyIds.CarlosOliveira1,
@@ -73,19 +71,57 @@ namespace IntelOrca.Biohazard.RE3
             return "jill.re3";
         }
 
-        public byte[] GetSlots(RandoConfig config, byte id)
-        {
-            return new byte[0];
-        }
-
         public bool IsNpc(byte type)
         {
             return type >= Re3EnemyIds.CarlosOliveira1 || type == Re3EnemyIds.MarvinBranagh1;
         }
 
+        public byte[] GetSlots(RandoConfig config, byte id)
+        {
+            switch (id)
+            {
+                case Re3EnemyIds.CarlosOliveira1:
+                    return new byte[] {
+                        Re3EnemyIds.MarvinBranagh1,
+                        Re3EnemyIds.CarlosOliveira1,
+                        Re3EnemyIds.NikolaiZinoviev,
+                        Re3EnemyIds.BradVickers,
+                        Re3EnemyIds.DarioRosso,
+                        Re3EnemyIds.MurphySeeker,
+                        Re3EnemyIds.TyrellPatrick,
+                        Re3EnemyIds.MarvinBranagh2,
+                        Re3EnemyIds.BradZombie,
+                        Re3EnemyIds.DarioZombie,
+                        Re3EnemyIds.PromoGirl,
+                        Re3EnemyIds.JillValentine1,
+                        Re3EnemyIds.JillValentine2,
+                        Re3EnemyIds.ChiefIrons
+                    };
+                default:
+                    return new[] { id };
+            }
+        }
+
         public bool IsSpareSlot(byte id)
         {
-            return false;
+            switch (id)
+            {
+                // Includes any NPC that does not have 15 parts
+                // case Re3EnemyIds.MarvinBranagh1:
+                case Re3EnemyIds.CarlosOliveira1:
+                case Re3EnemyIds.DarioRosso:
+                case Re3EnemyIds.MurphySeeker:
+                case Re3EnemyIds.MarvinBranagh2:
+                case Re3EnemyIds.BradZombie:
+                case Re3EnemyIds.DarioZombie:
+                case Re3EnemyIds.PromoGirl:
+                case Re3EnemyIds.JillValentine1:
+                case Re3EnemyIds.JillValentine2:
+                case Re3EnemyIds.ChiefIrons:
+                    return true;
+                default:
+                    return false;
+            }
         }
     }
 }
