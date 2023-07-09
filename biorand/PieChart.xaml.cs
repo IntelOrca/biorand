47ce74f01e1ffdfa39e9e48b8dde95faded84937
Ted John <ted@brambles.org>
2023-07-09 03:06:41 +0100

    Improve enemy bar chart

--- biorand/PieChart.xaml.cs
+++ biorand/PieChart.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -32,7 +33,7 @@ namespace IntelOrca.Biohazard.BioRand
         public void UpdatePie()
         {
             viewBox.Visibility = Visibility.Visible;
-            horizontalGrid.Visibility = Visibility.Collapsed;
+            topGrid.Visibility = Visibility.Collapsed;
 
             var gridItems = new List<UIElement>();
             var records = Records.Where(x => x.Value != 0).ToArray();
@@ -102,18 +103,38 @@ namespace IntelOrca.Biohazard.BioRand
         public void UpdateBar()
         {
             viewBox.Visibility = Visibility.Collapsed;
-            horizontalGrid.Visibility = Visibility.Visible;
+            topGrid.Visibility = Visibility.Visible;
+            topGrid.RowDefinitions.Clear();
+            topGrid.Children.Clear();
 
-            horizontalGrid.ColumnDefinitions.Clear();
-            horizontalGrid.Children.Clear();
+            var records = Records.OrderByDescending(x => x.Value).ToArray();
+            var split = DivideRecords(records);
 
+            var row = 0;
+            foreach (var s in split)
+            {
+                topGrid.RowDefinitions.Add(new RowDefinition()
+                {
+                    Height = new GridLength(18, GridUnitType.Pixel)
+                });
+
+                var g = CreateHorizontalBar(s);
+                Grid.SetRow(g, row);
+                topGrid.Children.Add(g);
+                row++;
+            }
+        }
+
+        private Grid CreateHorizontalBar(Record[] records)
+        {
+            var grid = new Grid();
             var index = 0;
-            foreach (var record in Records)
+            foreach (var record in records)
             {
                 if (record.Value == 0)
                     continue;
 
-                horizontalGrid.ColumnDefinitions.Add(new ColumnDefinition()
+                grid.ColumnDefinitions.Add(new ColumnDefinition()
                 {
                     Width = new GridLength(record.Value, GridUnitType.Star)
                 });
@@ -134,9 +155,84 @@ namespace IntelOrca.Biohazard.BioRand
 
                 border.Child = textBlock;
                 Grid.SetColumn(border, index);
-                horizontalGrid.Children.Add(border);
+                grid.Children.Add(border);
                 index++;
             }
+            return grid;
+        }
+
+        private Record[][] DivideRecords(Record[] records)
+        {
+            if (records.Length <= 3)
+                return new[] { records };
+
+            records = records.OrderByDescending(x => x.Value).ToArray();
+            var totalSize = records.Sum(x => x.Value);
+            var halfSize = totalSize / 2;
+            var left = FindOptimal((int)halfSize, records, out var splitLength);
+            if (left == null)
+            {
+                return new[] { records };
+            }
+            else
+            {
+                if (splitLength < halfSize)
+                {
+                    left = left.Append(new Record()
+                    {
+                        Name = "",
+                        Value = halfSize - splitLength,
+                        Color = Colors.Transparent
+                    }).ToArray();
+                }
+                var right = records.Except(left).ToArray();
+                var rightLength = right.Sum(x => (int)x.Value);
+                if (rightLength < halfSize)
+                {
+                    right = right.Append(new Record()
+                    {
+                        Name = "",
+                        Value = halfSize - rightLength,
+                        Color = Colors.Transparent
+                    }).ToArray();
+                }
+                return new[] { right, left };
+            }
+        }
+
+        private Record[] FindOptimal(int length, Record[] records, out int optimalLength)
+        {
+            Record[] optimal = null;
+            optimalLength = 0;
+            for (var i = 0; i < records.Length && optimalLength != length; i++)
+            {
+                var currentLength = (int)records[i].Value;
+                var remaining = length - currentLength;
+                if (remaining == 0)
+                {
+                    optimal = new[] { records[i] };
+                    optimalLength = length;
+                }
+                else if (remaining > 0)
+                {
+                    var rest = FindOptimal(remaining, records.Take(i).Concat(records.Skip(i + 1)).ToArray(), out var l);
+                    if (rest != null)
+                    {
+                        var total = currentLength + l;
+                        if (total > optimalLength)
+                        {
+                            optimal = new[] { records[i] }.Concat(rest).ToArray();
+                            optimalLength = total;
+                        }
+                    }
+                    else
+                    {
+                        optimal = new[] { records[i] };
+                        optimalLength = currentLength;
+                    }
+                }
+            }
+            return optimal;
         }
 
         private static TextBlock CreatePieLabel(Record record, Point position)
@@ -158,6 +254,7 @@ namespace IntelOrca.Biohazard.BioRand
             return ((color.R / 255.0) * 0.2126) + ((color.G / 255.0) * 0.7152) + ((color.B / 255.0) * 0.0722);
         }
 
+        [DebuggerDisplay("({Name}, {Value})")]
         public class Record
         {
             public string Name { get; set; }
